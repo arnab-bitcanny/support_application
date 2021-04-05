43d05e87dbb705e38af4ca3d70d2bbe93c6d8858
Arnab Roy <arnab.roy@bitcanny.com>
2021-04-05 21:02:38 +0530

    Using Turbo Stream

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,9 @@
 class Ticket < ApplicationRecord
   has_rich_text :content
   has_many :comments, dependent: :destroy
+
+  broadcasts
+  # after_create_commit ->  { broadcast_append_to self }
+  # after_destroy_commit ->  { broadcast_remove self }
+  # after_update_commit ->  { broadcast_replace self }
 end
