57eb192217cf8edb1bc922903f3c272ab01167fe
Arnab Roy <arnab.roy@bitcanny.com>
2021-04-05 21:16:33 +0530

    Adding turbo stream to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   has_rich_text :content
   belongs_to :ticket
+
+  broadcasts_to :ticket
+  # after_create_commit ->  { broadcast_append_to ticket }
+  # after_destroy_commit ->  { broadcast_remove ticket }
+  # after_update_commit ->  { broadcast_replace ticket }
 end
